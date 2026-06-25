7f4f1b36c6bbb7f427e580502fc80373b4f3757a
DomSantan <dominic@dsdatasolutions.com>
2026-06-25 11:20:05 +0100

    BoilerSparesUK: add scraper scaffold (middlewares.py)

---
+++ ScrapeProg/BoilerSparesUK/base/middlewares.py
@@ -0,0 +1,23 @@
+from scrapy import signals
+from itemadapter import is_item, ItemAdapter
+
+class BaseSpiderMiddleware:
+    @classmethod
+    def from_crawler(cls, crawler):
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_output(self, response, result, spider):
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
